8b4c4a4332635b71ed5e87510a91ea21a6487af2
vulla sai kiran kumar <vullasaikiran@gmail.com>
2023-09-12 22:38:18 +0530

    Add comman component in Cart and Payments

--- src/screens/PaymentScreen.js
+++ src/screens/PaymentScreen.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import Input from '../common/Input';
 
 const PaymentScreen = () => {
+  const [searchValue, setSearchValue] = useState();
   return (
     <View>
+      <Input
+        placeholderText="Search for Prevoius Payments"
+        intputValue={searchValue}
+        onChangeText={value => setSearchValue(value)}
+      />
       <Text>PaymentScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default PaymentScreen
+export default PaymentScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
